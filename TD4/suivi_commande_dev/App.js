fd6509af6b6e95b83dceed77349591fa75495e3b
apinot <antoine.pinot1@gmail.com>
2020-01-15 17:24:47 +0100

    oublie

--- TD4/suivi_commande_dev/App.js
+++ TD4/suivi_commande_dev/App.js
@@ -12,43 +12,14 @@ const PORT = 8080;
 const app = express();
 app.use(parser.json());
 
-// Middlewares
-app.use("/:request", function (req, res, next) {
-    const queries = Object.keys(req.query);
-    if (req.params.request !== "commandes"){
-        res.status(400).send(http.error(400))
-    }
-    else if(queries.length === 0){
-        next()
-    }
-    else if(queries.length === 1 && (queries[0] === 'statut' || queries[0] === 'page')){
-        next( )
-    }
-    else if(queries.length === 2 && queries.includes('size') && queries.includes('page')){
-        next()
-    }
-    else{
-        res.status(400).send(http.error(400))
-    }
-});
-
-app.use("/:request/:id", function (req, res, next) {
-    if (req.params.request !== "commandes"){
-        res.status(400).send(http.error(400))
-    }
-    else{
-        next()
-    }
-});
-
 
 /* ----------------- Routing  ----------------- */
 
 /******
- *
- * Méthode GET
- *
- ******/
+*
+* Méthode GET
+*
+******/
 
 // Root
 app.get("/", (req, res) => {
@@ -70,6 +41,14 @@ app.get("/commandes", (req, res) => {
     else{
         Collection.all().then(collection => { res.json(collection) })
     }
+});
+
+app.all('*', (req, res) => {
+    res.status(400).send(http.error(400))
+});
+
+app.use((error, req, res, next) => {
+    res.status(500).send(http.error(500))
 });
 
 app.listen(PORT);
