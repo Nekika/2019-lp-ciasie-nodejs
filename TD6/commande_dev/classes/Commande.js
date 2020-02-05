9ad2505684ae9c59e258c4933cec235a6c6b7533
felixleo22 <leofelixoff@outlook.fr>
2020-02-05 14:29:05 +0100

    td6

---
+++ TD6/commande_dev/classes/Commande.js
@@ -0,0 +1,101 @@
+const uuid = require('uuid');
+const date = require('date-and-time');
+
+const mysql = require("mysql");
+const db = mysql.createConnection({
+    host: "mysql.commande",
+    user: "command_lbs",
+    password: "command_lbs",
+    database: "command_lbs"
+});
+
+class Commande{
+    constructor(datas){
+        (datas.id) ? this.id = datas.id : this.id = uuid();
+        this.mail_client = datas.mail_client;
+        (datas.date_commande) ? this.date_commande = datas.date_commande : this.date_commande = date.format(new Date(), "YYYY-MM-DD HH:MM:SS");
+        (datas.date_livraison) ? this.date_livraison = datas.date_livraison : this.date_livraison = date.format(new Date(), "YYYY-MM-DD HH:MM:SS");
+        this.montant = datas.montant;
+        this.statut = datas.statut;
+    }
+
+    static all(){
+        return new Promise((resolve, reject) => {
+            const sql = "SELECT * FROM commande ORDER BY date_commande ASC";
+            db.query(sql, (error, result) => {
+                if (!error){
+                    resolve(result)
+                }
+                else{
+                    reject(error)
+                }
+            })
+        })
+    }
+
+    static find(id){
+        return new Promise((resolve, reject) => {
+            const sql = "SELECT * FROM commande WHERE id = ?";
+            db.query(sql, id, (error, result) => {
+                if (!error){
+                    resolve(result[0])
+                }
+                else{
+                    reject(error)
+                }
+            })
+        })
+    }
+
+    static findByStatut(statut){
+        return new Promise((resolve, reject) => {
+            const sql = "SELECT * FROM commande WHERE statut = ? ORDER BY date_commande ASC";
+            db.query(sql, statut, (error, result) => {
+                if (!error){
+                    console.log(result);
+                    resolve(result)
+                }
+                else{
+                    reject(error)
+                }
+            })
+        })
+
+    }
+
+    save(){
+        return new Promise((resolve, reject) => {
+            const sql = "INSERT INTO commande SET ?";
+            db.query(sql, this, (error) => {
+                if (!error){
+                    resolve()
+                }
+                else{
+                    reject(error)
+                }
+            })
+        })
+    }
+
+    update(datas){
+        for (let data in datas){
+            if (this.hasOwnProperty(data)){
+                this[data] = datas[data]
+            }
+        }
+        return new Promise((resolve, reject) => {
+            const sql = "UPDATE commande SET mail_client = ?, date_livraison = ?, montant = ?, statut = ? WHERE id = ?";
+            const values = [this.mail_client, this.date_livraison, this.montant, this.statut, this.id]
+            db.query(sql, values, (error) => {
+                if(!error){
+                    resolve(this)
+                }
+                else{
+                    reject(error)
+                }
+            })
+        })
+    }
+}
+
+module.exports = Commande;
