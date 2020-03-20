ec4c404b41e912935eaefcd74328bdbe5a02f378
apinot <antoine.pinot1@gmail.com>
2020-03-20 19:45:04 +0100

    catalogue doc

--- TD8/catalogue_dev/catalogue_doc/api_project.js
+++ TD8/catalogue_dev/catalogue_doc/api_project.js
@@ -1,14 +1,14 @@
 define({
-  "name": "example",
+  "name": "Catalogue",
   "version": "0.1.0",
-  "description": "apiDoc basic example",
-  "title": "Custom apiDoc browser title",
+  "description": "api du service catalogue",
+  "title": "Doc - API Catalogue",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-03-20T16:21:31.631Z",
+    "time": "2020-03-20T18:42:50.742Z",
     "url": "http://apidocjs.com",
     "version": "0.20.1"
   }
